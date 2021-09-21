b7e3bd3a6edf7936860b9b6526737b8d9179be94
KhunNooDev <noo_okok@hotmail.com>
2021-09-21 19:37:54 +0700

    test fail pipeline

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const port = process.env.port || 8080
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello world!!!')
+  res.send('Hello world!')
 })
 
 app.listen(port, () => {
-    console.log('The app is up and running!')
+  console.log('The app is up and running!')
 })
 module.exports = app
