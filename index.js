e2c1be14ad83d2f46c036101c65223bc61a32116
KhunNooDev <noo_okok@hotmail.com>
2021-09-23 22:30:36 +0700

    Exercise

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ app.get('/', (req, res) => {
   res.send('Hello world!!!')
 })
 
+app.get('/users', (req, res) => {
+  res.send('No user in the system')
+})
+
 app.listen(port, () => {
   console.log('The app is up and running!')
 })
+
 module.exports = app
